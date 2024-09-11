edf770ee83b4ce94c9c966731c9186ca18315bdc
Cas Huurdeman <cashuurdeman@gmail.com>
2024-09-11 16:54:16 +0200

    Added 'Aantekeningen_les'2 and 'playsound.py'

---
+++ CSD2a/python_basics/playsound.py
@@ -0,0 +1,25 @@
+import pygame
+import time
+
+pygame.init()
+
+rhythm = []
+
+# Rhythm list contains delays in seconds
+rhythm.append(1)
+rhythm.append(0.5)
+rhythm.append(1)
+
+Hihat = pygame.mixer.Sound('Hihat.wav')
+
+for delay in rhythm:
+    play_obj = Hihat.play()
+    time.sleep(delay)
+
+numberOfRepeats = int(input("Aantal herhalingen: "))
+
+for i in range(numberOfRepeats):
+    play_obj = Hihat.play()
+    time.sleep(0.5)
+
+play_obj.stop()
