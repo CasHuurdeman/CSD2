bf38cebca5b673591dfe63b28f7edc6c2f454206
Cas Huurdeman <cashuurdeman@gmail.com>
2024-09-13 12:28:06 +0200

    Expanded and modified 'ritmischePlayback.py'

--- CSD2a/python_basics/ritmischePlayback.py
+++ CSD2a/python_basics/ritmischePlayback.py
@@ -5,28 +5,39 @@ pygame.init()
 
 Hihat = pygame.mixer.Sound('Hihat.wav')
 
-# Rhythm list contains delay in note values (1 for quarter note)
+# Rhythm list contains delay in seconds
 rhythm = []
 BPM = int(input("BPM: "))
 numPlaybackTimes = int(input("How many notes: "))
 
-# inputNumber = 1
-# 
-# for i in range(numPlaybackTimes) :
-#     rhythm.append(input("Rhythm input ", inputNumber, ": "))
-#     inputNumber+=1
+inputNumber = 1
 
+bpmToSeconds = 60/BPM
 
-BPM_toSeconds = 60/BPM  #hoe doe je dit in camelcap
+print("Type 1 for a quarter note, 0.5 for an eight note etc.")
 
-print("Type 1 for a quarter note, 0.5 for an eight note etc.: ")
-      
+#calculates and pushes the note values in seconds into rhythm     
 for i in range(numPlaybackTimes) :
-    rhythm.append(float(input("Rhythm input: ")) * BPM_toSeconds )
+    rhythm.append(float(input(f"Rhythm input {inputNumber}: ")) * bpmToSeconds )
+    inputNumber+=1
+
+# loop functie
+loop = input("Do you want to (infinitly) loop this rhythm? (yes/no):")
+
+if loop == "yes":
+    loopBool = True
+else: loopBool = False
 
 
-for delay in rhythm:
-    play_obj = Hihat.play()
-    time.sleep(delay)
+while loopBool == True :
+    for delay in rhythm:
+        play_obj = Hihat.play()
+        time.sleep(delay)
+else:    
+    for delay in rhythm:
+        play_obj = Hihat.play()
+        time.sleep(delay)
+
+    
 
 rhythm.clear()
